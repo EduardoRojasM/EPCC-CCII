c4ff697c05b3fdf9f7573149d8b28394aa89fb08
Eduardo Rojas <erojasm@unsa.edu.pe>
2022-07-24 20:15:52 -0500

    Commit

--- EjerciciosClase/TerceraUnidad/Semana15/1. e1multiplicar/e1multiplicar.cpp
+++ EjerciciosClase/TerceraUnidad/Semana15/1. e1multiplicar/e1multiplicar.cpp
@@ -1,3 +1,20 @@
+/*
+1. e1multiplicar.cpp.
+Implemente una función que reciba dos parámetros, una matriz "x" y un
+valor, que multiplique los elementos de dicha matriz por ese valor.
+
+int x[3][4][2] = {{{1, 2}, {4, 5}, {7, 8}, {1, 2}},
+{{10, 11}, {13, 14}, {16, 17}, {1, 2}},
+{{19, 20}, {22, 23}, {25, 26}, {1, 2}}};
+
+La implementación debe funcionar para cualquier tamaño de matriz
+tridimensional.
+La implementación debe deducir el tamaño para cada dimensión.
+Forma de llamar a la función multiplicar(x, 4); significa que cada
+elemento de la matriz se multiplicará por 4.
+Implemente además una función para imprimir la matriz tridimensional.
+*/
+
 #include <iostream>
 
 using namespace std;
--- EjerciciosClase/TerceraUnidad/Semana15/2. e2funcion/e2funcion.cpp
+++ EjerciciosClase/TerceraUnidad/Semana15/2. e2funcion/e2funcion.cpp
@@ -1,8 +1,29 @@
+/*
+2. e2funcion.cpp.
+Escriba una función de tal forma que usted pueda invocar a cualquiera de
+las funciones siguientes.
+
+float sumafi(float a, int b) {
+return a + b;
+}
+double restalf(float a, double b) {
+return a - b;
+}
+long multiplicaid(int a, long b) {
+return a * b;
+}
+
+Forma de llamar a la función:
+funcion(sumafi, 4.0f, 5);
+funcion(restalf, 4.0f, 5.0);
+funcion(multiplicaid, 4, 5L);
+*/
+
 #include <iostream>
 
 using namespace std;
 
-float sumafi(float a, int b) {
+float sumafi(int a, float b) {
     return a + b;
 }
 
@@ -14,12 +35,20 @@ long multiplicaid(int a, long b) {
     return a * b;
 }
 
-void funcion(int a, int b){
-    cout<<sumafi(a,b);
+template <class A, class B>
+B funcion(B (func)(A, B),A a, B b){
+    return func(a,b);
 }
 
 int main(){
-    funcion(sumafi,2,3);
+    //Llamada de la funciones
+    funcion(sumafi, 5, 4.0f);
+    funcion(restalf, 4.0f, 5.0);
+    funcion(multiplicaid, 4, 5L);
+    //Comprobacion imprimiendo
+    cout<<funcion(sumafi, 5, 4.0f)<<endl;
+    cout<<funcion(restalf, 4.0f, 5.0)<<endl;
+    cout<<funcion(multiplicaid, 4, 5L)<<endl;
 
     return 0;
 }
